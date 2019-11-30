5100395624e5b43e8676ef6dc6284bdb0348a043
Phạm Đức <minhduc.096.99@gmail.com>
2019-11-30 19:42:21 +0700

    .

---
+++ get-over-it.js
@@ -0,0 +1,11 @@
+const signUpForm = document.getElementById('form-signup')
+
+signUpForm.addEventListener('submit', function() {
+  const email = document.getElementById('email-input')
+  const password = document.getElementById('password-input')
+  const passwordConfirm = document.getElementById('password-confirm')
+  console.log(email)
+  console.log(password)
+  console.log(passwordConfirm)
+
+})
