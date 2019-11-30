d39d067d4d373ad24131b80eb653249245ddc2ba
Phạm Đức <minhduc.096.99@gmail.com>
2019-11-30 20:32:12 +0700

    .

--- get-over-it.js
+++ get-over-it.js
@@ -1,6 +1,6 @@
 const signUpForm = document.getElementById('form-signup')
 
-signUpForm.addEventListener('submit', function(event) {
+signUpForm.addEventListener('submit',async function(event) {
   event.preventDefault();
   const email = document.getElementById('email-input').value
   const password = document.getElementById('password-input').value
@@ -12,7 +12,20 @@ signUpForm.addEventListener('submit', function(event) {
     const errMessageContainer = document.getElementById('error-message')
     errMessageContainer.innerText = "Mật khẩu không khớp, nhập lại đê bạn eii"
   } else {
-    
+    const data = {
+      username: email,
+      password: password,
+    }
+    const url = "http://5dc6a9cb317717001434f796.mockapi.io/api/users"
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+    const data = await response.json()
+    return data
   }
 
 })
